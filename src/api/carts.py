e07b0236874ab532fca307c246da080a5bb04a35
jwu205 <jinwu2003@gmail.com>
2023-10-02 11:34:53 -0700

    updated cart checkout to add correct payment

--- src/api/carts.py
+++ src/api/carts.py
@@ -39,7 +39,7 @@ def create_cart(new_cart: NewCart):
 @router.get("/{cart_id}")
 def get_cart(cart_id: int):
     """ """
-    return {"cart id = " + str(cart_id)}
+    return {}
 
 
 class CartItem(BaseModel):
@@ -58,16 +58,17 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
-    # obtaining item quantity and item_sku
+    # obtaining item quantity, item_sku and expected payment
     item_quantity = total_carts[cart_id].quantity
     item_sku = total_carts[cart_id].item_sku
+    expected_payment = item_quantity * 50
     # checking for red potion purchase
     if item_sku == "RED_POTION_0":
         with db.engine.begin() as connection:
             # figuring out new gold and red potion count
             result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
             first_row = result.first()
-            total_gold = first_row.gold + int(cart_checkout.payment)
+            total_gold = first_row.gold + expected_payment
             total_red_potions =  first_row.num_red_potions - item_quantity
             # update gold and potion count in DB
             connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = " + str(int(total_gold))))
@@ -75,4 +76,4 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     # deleting cart from dictionary
     del total_carts[cart_id]
 
-    return {"total_potions_bought": int(item_quantity), "total_gold_paid": int(cart_checkout.payment)}
+    return {"total_potions_bought": int(item_quantity), "total_gold_paid": int(expected_payment)}
