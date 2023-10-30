cf1ded1f4d554cb6a6258e72e5991157d63b5ead
jwu205 <jinwu2003@gmail.com>
2023-10-29 22:08:28 -0700

    admin functions

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,6 +16,16 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        connection.execute(
+            sqlalchemy.delete(db.cart_items),
+            sqlalchemy.delete(db.gold_ledger_entities),
+            sqlalchemy.delete(db.ml_ledger_entities),
+            sqlalchemy.delete(db.potion_ledger_entities),
+            )
+        connection.execute(
+            sqlalchemy.insert(db.gold_ledger_entities).values(gold_change = 100, description = "intitial population")
+        )
     return "OK"
 
 
@@ -23,7 +35,7 @@ def get_shop_info():
 
     # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "Potions o' Plenty",
+        "shop_owner": "Jin Wu",
     }
 
